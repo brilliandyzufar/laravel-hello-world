ae24ffb4e35cf7f3aef0e8105bf59c7f6d153885
= <=>
2021-02-18 12:30:43 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+//Praktikum 1
+Route::get('/', function ($id) {
     return view('welcome');
+});
+//no 1
+Route::any('/', function () {
+     return "Selamat Datang";
+});
+//no 2
+Route::any('/about', function () {
+     return "Nama: Brilliandy zufar P <p> NIM: 1941720201 <p> Kelas: TI-2A";
+    
+});
+//no 3
+Route::get('/article/{id}', function ($id) {
+     return "Halaman artikel id " .$id;
+
 });
