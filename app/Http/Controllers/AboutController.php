a2209a940a2a0f79fb4b2474ce0b6d5c239434ed
= <=>
2021-02-18 12:46:59 +0700

    routes & controller

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -1,5 +1,5 @@
 <?php
-
+//AboutController
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,5 +1,5 @@
 <?php
-
+//PageController
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
